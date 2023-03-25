bb54ceba3f6611f15c4b950c03f05f3adebda1bf
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-25 12:01:15 -0700

    header and footer add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Countries from './Component/Countries/Countries';
+import Footer from './Component/Footer/Footer';
+import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Countries></Countries>
+      <Footer></Footer>
     </div>
   );
 }
