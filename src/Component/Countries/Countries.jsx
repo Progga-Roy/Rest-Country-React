880a2a6f6e6021fbb6697e51e9c69c4bc512fda0
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-25 11:38:10 -0700

    unique key add for avoid warning

--- src/Component/Countries/Countries.jsx
+++ src/Component/Countries/Countries.jsx
@@ -15,7 +15,7 @@ const Countries = () => {
             <p>Available Countries : {countries.length}</p>
            <div className="countries">
            {
-            countries.map(country=><Country country={country} ></Country>)
+            countries.map(country=><Country country={country} key={country.cca3} ></Country>)
             }
            </div>
         </div>
